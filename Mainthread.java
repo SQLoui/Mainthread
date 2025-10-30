0c5a486124c804bab1b4c656be64a5052b4ac5c8
SQLoui <luiguipro5@gmail.com>
2025-10-29 18:50:43 -0600

    Inicializacion
    inicialisazion de hilo en Main

--- Mainthread.java
+++ Mainthread.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Mainthread
 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String[] info = new String[30];
+        Posicion pos = new Posicion();
+
+		HiloEscribe h1 = new HiloEscribe(pos);
+        h1.setInfo(info);
+        h1.setIni(0);
+        h1.setTmp(500);
 		
+		System.out.println("Iniciando hilo...");
+        h1.start();
+
+        System.out.println("\n Arreglo:");
+        for (int i = 0; i < info.length; i++) {
+        	System.out.println(i + ": " + info[i]);
+        }
 	}
 }
